1bca49493ad56f1fa682b1e63010873e54e2a558
Pavel Evtushenko <whatisclamp@gmail.com>
2022-10-17 15:50:00 +0300

    feat: change img files to .webp

--- src/components/Aside/Avatar/Avatar.jsx
+++ src/components/Aside/Avatar/Avatar.jsx
@@ -1,9 +1,9 @@
-const Avatar = ({avatar}) => {
-    return (
-        <div className='avatar'>
-            <img src={avatar} alt="Avatar"/>
-        </div>
-    );
+const Avatar = ({ avatar }) => {
+  return (
+    <div className="avatar">
+      <img src={avatar} alt="Avatar" />
+    </div>
+  );
 };
 
 export default Avatar;
